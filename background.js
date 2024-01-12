97f16f7864eb591b2d3c9583ef8f9a5a01fa2e0f
Valen DeLeon <valeriedeleon4521@gmail.com>
2024-01-12 15:07:48 -0800

    adds functionality

--- background.js
+++ background.js
@@ -1,6 +1,36 @@
+const docs = "https://www.w3schools.com/";
+
 // set extension as off by default
 chrome.runtime.onInstalled.addListener(() => {
     chrome.action.setBadgeText({
-        text: "Off",
+        text: "OFF",
     });
+});
+
+chrome.action.onClicked.addListener(async (tab) => {
+    if (tab.url.startsWith(docs))
+    {
+        const prevState = await chrome.action.getBadgeText({ tabId: tab.id });
+        const nextState = prevState === 'ON' ? 'OFF' : 'ON';
+
+        await chrome.action.setBadgeText({
+            tabId: tab.id,
+            text: nextState,
+        });
+
+        if (nextState === "ON")
+        {
+            await chrome.scripting.insertCSS({
+                files: ["w3schools-focus.css"],
+                target: { tabId: tab.id },
+            });
+        }
+        else if (nextState === "OFF")
+        {
+            await chrome.scripting.removeCSS({
+                files: ["w3schools-focus.css"],
+                target: { tabId: tab.id },
+            });
+        }
+    }
 });
